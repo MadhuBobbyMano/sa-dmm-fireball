6f13d7936a895544e3aa2af22d87e59a4b06193d
mbala <MadhuChandrika_Bala@premierinc.com>
2024-08-30 12:55:30 +0530

    AB#1107325 Landing page and view implementation

--- dmm/src/main/resources/static/scripts/main.js
+++ dmm/src/main/resources/static/scripts/main.js
@@ -1,12 +1,18 @@
 require([
     "common-ui/controllers/_NavigationController",
+    "agGridComponents/editMarkUp",
     "app.json"
-], function(NavigationController, appConfig) {
+], function(NavigationController,
+            editMarkUp,
+            appConfig) {
 
     var app = new NavigationController({
         appConfig : appConfig,
         customWidgetConstructors : {
-
+            AGGrid: require("./common-ui/widgets/grid/AGGrid"),
+        },
+        agGridComponents: {
+            editMarkUp
         }
     });
 
