b3d5d75cd2dbbb2b092fc3f85b7eb4e00dbac8c5
ljayakum <Leena_Jayakumar@premierinc.com>
2024-08-30 13:05:57 +0530

    AB#1107425 Without Markup page implementation

--- dmm/src/main/resources/static/scripts/main.js
+++ dmm/src/main/resources/static/scripts/main.js
@@ -1,13 +1,21 @@
 require([
     "common-ui/controllers/_NavigationController",
+    "common-ui/widgets/grid/AGGrid",
+    "agGridComponents/withoutMarkup",
     "app.json"
-], function(NavigationController, appConfig) {
+], function(NavigationController,
+            AGGrid,
+            withoutMarkup,
+            appConfig) {
 
     var app = new NavigationController({
         appConfig : appConfig,
         customWidgetConstructors : {
-
-        }
+            AGGrid : require("./common-ui/widgets/grid/AGGrid")
+        },
+        agGridComponents : {
+            withoutMarkup
+        },
     });
 
 });
